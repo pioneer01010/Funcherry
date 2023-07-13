551095fc9eaaa97b7b52620880cd9bc29d9e460d
pioneer01010 <pioneer020@gmail.com>
2023-07-13 11:41:12 +0900

    Setup skeleton code for pytest env.

---
+++ src/funcherry/funcherry.py
@@ -0,0 +1,4 @@
+class Funcherry:
+    def __init__(self, signature: str):
+        self.signature = signature
+
---
+++ tests/test_funcherry.py
@@ -0,0 +1,5 @@
+from funcherry import Funcherry
+
+class TestFuncherry:
+    def test_funcherry(self):
+        funcherry = Funcherry("signature")
