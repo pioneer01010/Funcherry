fdf1830a4c83fdfdab3a657a2dcbb238e530232b
pioneer01010 <pioneer020@gmail.com>
2023-08-03 17:03:05 +0900

    Generate sample json data

--- src/datasets/preproces.py
+++ src/datasets/preproces.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 from utils.git import GitClient
 from utils.parser import FunctionParser
@@ -13,14 +14,20 @@ def main():
     path = "C:\\/Users\\/yc_pine.hong\\/Downloads\\/Funcherry"
 
     client = GitClient.clone(repo, path)
+
     for rev in client.rev_list(reverse=True):
         client.checkout(rev)
+        func_dict = []
+        rev_dict = {"revision": rev}
 
         files = [f for f in Path(path).rglob('*.py') if not f.name.startswith("__init__")]
         for file in files:
             functions = FunctionParser.parse_functions_from_file(file)
-            for func in functions:
-                logging.info(func)
+            func_dict.append(functions)
+
+        rev_dict["functions"] = func_dict
+        with open('sample/' + rev + '.json', 'w') as json_file:
+            json.dump(rev_dict, json_file)
 
 
 if __name__ == '__main__':
